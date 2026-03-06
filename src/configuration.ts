3fbfdcfad5e6d8b0ee2deab66178586819a05de0
badong <gautier.bardon@gmail.com>
2026-03-06 21:21:47 +0100

    Add button

---
+++ src/configuration.ts
@@ -0,0 +1,7 @@
+declare module 'fvtt-types/configuration' {
+  namespace Hooks {
+    interface ApplicationConfig {
+      CharacterSheetPF2e: foundry.appv1.api.Application.Any;
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
 import './styles/index.scss';
 
+// CONFIG.debug.hooks = !CONFIG.debug.hooks;
+// console.warn('Set Hook Debugging to', CONFIG.debug.hooks);
+
 console.log('init module2');
+
+Hooks.once('init', () => {
+  console.debug('pf2e mobile startss');
+});
+
+/**
+ * Add a new button that displays the sidebar information in a dedicated page
+ */
+Hooks.on('renderCharacterSheetPF2e', (app, html, data) => {
+  const navigationRow = html.get(0)?.querySelector('.sheet-navigation');
+  const activeItem = navigationRow?.querySelector('.active');
+  const characterRecapItem = activeItem?.cloneNode(true) as Element;
+  characterRecapItem.classList.remove('active');
+  navigationRow?.insertBefore(characterRecapItem, navigationRow.childNodes[2]);
+});
