5f998d9c0c1d202fabc454eaf6641ed787909fb0
badong <gautier.bardon@gmail.com>
2026-03-19 00:33:13 +0100

    WIP vertical align top - incredible behavior with text-less divs aligning to the baseline

--- src/main.ts
+++ src/main.ts
@@ -17,4 +17,19 @@ Hooks.once('init', () => {
 Hooks.on('renderCharacterSheetPF2e', async (_app, html, _data) => {
   console.debug('handleMobileSidebar');
   handleMobileSidebar(html);
+  /**
+   * Make sheet sidebar vertical
+   */
+
+  // Move duplciate sidebar (will be completel moved when dev is over)
+  const parsedHtml = html.get(0);
+  const sheetNavigationElement = parsedHtml
+    ?.querySelector('.window-content form.editable')
+    ?.removeChild(
+      html.get(0)?.querySelector('.sheet-navigation') as HTMLElement,
+    );
+
+  parsedHtml
+    ?.querySelector('.sheet-body')
+    ?.appendChild(sheetNavigationElement as HTMLElement);
 });
