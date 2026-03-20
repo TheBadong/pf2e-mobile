2c33d0dbbfcf8e9945a0dcc9b6c1ae2e8999145d
badong <gautier.bardon@gmail.com>
2026-03-20 20:12:25 +0100

    working sides drag n drop

--- src/navigation-management.ts
+++ src/navigation-management.ts
@@ -7,8 +7,14 @@
  * In-memory state.
  * Eventually the final navigation state will be store somewhere in the app. Localstorage maybe?
  */
-const navigationBarState = {
+const navigationBarState: {
+  defaultPosition: 'left';
+  position: 'left' | 'right';
+  isBeingDragged: boolean;
+  dropTarget: 'left' | 'right' | null;
+} = {
   defaultPosition: 'left',
+  position: 'left',
   isBeingDragged: false,
   dropTarget: null,
 };
@@ -20,57 +26,78 @@ export function handleCharacterNavigation(html: JQuery<HTMLElement>) {
 
   // Move navigation (will be completely moved when dev is over)
   const parsedHtml = html.get(0) as HTMLElement;
+  const sheetBody = parsedHtml.querySelector('.sheet-body') as HTMLElement;
+  const sheetContent = parsedHtml.querySelector(
+    '.sheet-content',
+  ) as HTMLElement;
+
   const sheetNavigationElement = parsedHtml
     ?.querySelector('.window-content form.editable')
     ?.removeChild(
       html.get(0)?.querySelector('.sheet-navigation') as HTMLElement,
     ) as HTMLElement;
 
-  parsedHtml
-    ?.querySelector('.sheet-body')
-    ?.insertBefore(
-      sheetNavigationElement as HTMLElement,
-      parsedHtml.querySelector('.sheet-content') as HTMLElement,
-    );
+  sheetBody.insertBefore(sheetNavigationElement, sheetContent);
 
   // Handle drag
+  const dropZones = document.createElement('div');
+  dropZones.style.display = 'none';
+  dropZones.style.position = 'absolute';
+  dropZones.style.top = '0px';
+  dropZones.style.width = '100vw';
+  dropZones.style.height = '100vh';
+
+  const dropZoneLeft = document.createElement('div') as HTMLElement;
+  dropZoneLeft.style.display = 'inline-block';
+  dropZoneLeft.style.width = '50vw';
+  dropZoneLeft.style.height = '100vh';
+  const dropZoneRight = dropZoneLeft.cloneNode() as HTMLElement;
+
+  // Mark sheet body as drag target
+  sheetBody.ondragover = (e) => e.preventDefault();
+  sheetBody.ondrop = (e: DragEvent) => {
+    console.log('dropped on target ', navigationBarState.dropTarget);
+    console.log('target', e.target, e);
+    switch (navigationBarState.dropTarget) {
+      case 'left':
+        if (navigationBarState.position === 'right') {
+          navigationBarState.position = 'left';
+          sheetNavigationElement.style.order = '-1';
+        }
+        break;
+      case 'right':
+        if (navigationBarState.position === 'left') {
+          navigationBarState.position = 'right';
+          sheetNavigationElement.style.order = '1';
+        }
+        break;
+    }
+  };
+
+  dropZoneRight.ondragenter = (e: DragEvent) => {
+    console.debug('Entered right drop zone');
+    navigationBarState.dropTarget = 'right';
+  };
+  dropZoneLeft.ondragenter = (e: DragEvent) => {
+    console.debug('Entered left drop zone');
+    navigationBarState.dropTarget = 'left';
+  };
+
+  dropZones.appendChild(dropZoneLeft);
+  dropZones.appendChild(dropZoneRight);
+
+  sheetContent.appendChild(dropZones);
+
   sheetNavigationElement.draggable = true;
   sheetNavigationElement.ondragstart = (e: DragEvent) => {
     console.debug('drag start!');
-    // Create drop zones
+    dropZones.style.display = 'block';
   };
 
   sheetNavigationElement.ondragend = (e: DragEvent) => {
     console.debug('drag end!');
-    //document.body.removeChild(dropZones);
+    dropZones.style.display = 'none';
   };
-  // let dragTimeout: NodeJS.Timeout;
-  // dragTimeout = setTimeout(() => {
-  //   navigationBarState.isBeingDragged = true;
-  //   console.debug('is being inited');
-  // }, 1000);
-  // sheetNavigationElement.addEventListener('pointerdown', (e: PointerEvent) => {
-  //   dragTimeout = setTimeout(() => {
-  //     navigationBarState.isBeingDragged = true;
-  //     console.debug('is being dragged');
-  //   }, 1000);
-  // });
-
-  // sheetNavigationElement.addEventListener('pointermove', (e: PointerEvent) => {
-  //   if (navigationBarState.isBeingDragged) {
-  //     const draggedElement = sheetNavigationElement.cloneNode() as HTMLElement;
-  //     console.debug('draggedElement');
-  //     console.debug('draggine elem to: ', e.x, e.y);
-
-  //     draggedElement.style.position = 'absolute';
-  //     draggedElement.style.zIndex = '100';
-  //     draggedElement.style.left = e.x + 'px';
-  //     draggedElement.style.top = e.y + 'px';
-  //   }
-  // });
-
-  // sheetNavigationElement.addEventListener('pointerup', (e: PointerEvent) => {
-  //   navigationBarState.isBeingDragged = false;
-  //   clearTimeout(dragTimeout);
-  // });
 }
+
+function previewDrop(): void {}
