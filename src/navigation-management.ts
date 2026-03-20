e7cb8e1038b972db96ace1fbd04138e598b95310
badong <gautier.bardon@gmail.com>
2026-03-20 13:13:57 +0100

    add basic drag detection

--- src/navigation-management.ts
+++ src/navigation-management.ts
@@ -2,18 +2,29 @@
  * Handle vertical navigation bar.
  * It will be possible to drag the navigation bar to fixed positions : up, right and left
  */
+
+/**
+ * In-memory state.
+ * Eventually the final navigation state will be store somewhere in the app. Localstorage maybe?
+ */
+const navigationBarState = {
+  defaultPosition: 'left',
+  isBeingDragged: false,
+  dropTarget: null,
+};
+
 export function handleCharacterNavigation(html: JQuery<HTMLElement>) {
   document
     .querySelector('.character.sheet form')
     ?.classList.add('navigation-sides');
 
   // Move navigation (will be completely moved when dev is over)
-  const parsedHtml = html.get(0);
+  const parsedHtml = html.get(0) as HTMLElement;
   const sheetNavigationElement = parsedHtml
     ?.querySelector('.window-content form.editable')
     ?.removeChild(
       html.get(0)?.querySelector('.sheet-navigation') as HTMLElement,
-    );
+    ) as HTMLElement;
 
   parsedHtml
     ?.querySelector('.sheet-body')
@@ -21,4 +32,22 @@ export function handleCharacterNavigation(html: JQuery<HTMLElement>) {
       sheetNavigationElement as HTMLElement,
       parsedHtml.querySelector('.sheet-content') as HTMLElement,
     );
+
+  // Handle drag
+  let dragTimeout: NodeJS.Timeout;
+  dragTimeout = setTimeout(() => {
+    navigationBarState.isBeingDragged = true;
+    console.debug('is being inited');
+  }, 1000);
+  sheetNavigationElement.addEventListener('pointerdown', (e: MouseEvent) => {
+    dragTimeout = setTimeout(() => {
+      navigationBarState.isBeingDragged = true;
+      console.debug('is being dragged');
+    }, 1000);
+  });
+
+  sheetNavigationElement.addEventListener('pointerup', (e: MouseEvent) => {
+    navigationBarState.isBeingDragged = false;
+    clearTimeout(dragTimeout);
+  });
 }
