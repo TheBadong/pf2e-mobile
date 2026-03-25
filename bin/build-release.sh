ba84b581a85efbfaab96c3f70e912325131963bf
badong <gautier.bardon@gmail.com>
2026-03-25 18:31:01 +0100

    test auto release version

--- bin/build-release.sh
+++ bin/build-release.sh
@@ -1,3 +1,7 @@
+# Read release version from dist module.json
+VERSION="$(jq -r '.version' module.json)"
+echo "Preparing release for version $VERSION";
+
 mkdir release
 cp -R ./dist release/pf2e-mobile
 cp public/module.json release/module.json
@@ -5,5 +9,5 @@ cd release
 zip -r module.zip ./pf2e-mobile
 rm -rf ./pf2e-mobile
 cd ../
-gh release create 0.4.1 "./release/*" --draft --title "PF2E Mobile 0.4.1"
+gh release create $VERSION "./release/*" --draft --title "PF2E Mobile $VERSION"
 rm -rf ./release
