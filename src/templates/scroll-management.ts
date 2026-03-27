29ea7b443d45288284fa2b31325d0be0aa17d97c
badong <gautier.bardon@gmail.com>
2026-03-27 19:08:00 +0100

    handle scroll nav click

--- src/templates/scroll-management.ts
+++ src/templates/scroll-management.ts
@@ -11,6 +11,10 @@ export function handleScroll(html: JQuery<HTMLElement>) {
     '.sheet-content',
   ) as HTMLElement;
 
+  const sheetNavigation = parsedHtml.querySelector(
+    '.sheet-navigation',
+  ) as HTMLElement;
+
   // Add active on all elements (probably a bad idea)
   for (const child of sheetContent.children) {
     child.classList.add('active');
@@ -23,4 +27,32 @@ export function handleScroll(html: JQuery<HTMLElement>) {
 
   // Instantly scroll to the last registered scroll position
   sheetContent.scroll({ top: registeredScroll, left: 0, behavior: 'instant' });
+
+  // Prevent all other click events on the navbar
+  sheetNavigation.addEventListener(
+    'click',
+    (e) => {
+      e.stopPropagation();
+      console.debug('clicked');
+      const targetTabName = (e.target as HTMLElement)
+        ?.closest('a.item')
+        ?.getAttribute('data-tab');
+      if (!targetTabName) return;
+
+      const targetSection = sheetContent.querySelector(
+        `[data-tab="${targetTabName}"]`,
+      ) as HTMLElement | null;
+
+      if (!targetSection) {
+        console.error(`Could not scroll to ${targetTabName}! No such tab.`);
+      }
+
+      sheetContent.scrollTo({
+        top: targetSection?.offsetTop,
+        left: 0,
+        behavior: 'instant',
+      });
+    },
+    { capture: true },
+  );
 }
